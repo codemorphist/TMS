0d82ebf09c123b36bf6601d39db2375ac8c9d38d
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-04-06 22:30:51 +0300

    Implemented base.html template
    - Implemented base.html templated
    - Refactored other templates with base.html template

--- panel/urls.py
+++ panel/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 import panel.views as views
 
 urlpatterns = [
-    path('', views.panel),
+    path('', views.panel, name='panel'),
     path('products/', views.show_products, name='products'),
     path('product/<int:product_id>', views.show_product, name='product'),
 ]
