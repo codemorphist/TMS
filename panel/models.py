95793eb7af26ec00821bba26eae5575b7c9ce22e
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-04-06 15:12:55 +0300

    Added Product model
    - Added Product model
    - Maked migrations
    - Migrated database

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.FloatField(default=0.0)
+    count = models.IntegerField(default=0)
