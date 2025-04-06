ccf7b039b2d50fddee70ea444b09d41ace45175c
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-04-06 20:23:55 +0300

    Implemented product show list
    - Added path to show all products
    - Added path to show product info by id
    - Added page to show all products
    - Added page to show product info

--- panel/urls.py
+++ panel/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 import panel.views as views
 
 urlpatterns = [
-    path('', views.panel)
+    path('', views.panel),
+    path('products/', views.show_products, name='products'),
+    path('product/<int:product_id>', views.show_product, name='product'),
 ]
 
 
--- panel/views.py
+++ panel/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from panel.models import Product
 
 
 def panel(request: HttpRequest) -> HttpResponse:
@@ -9,3 +11,23 @@ def panel(request: HttpRequest) -> HttpResponse:
     return render(request, 'panel/panel.html')
 
 
+def show_products(request: HttpRequest) -> HttpResponse:
+    """
+    Products main page, that show table of all products
+    """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'panel/products.html', context=context)
+
+
+def show_product(request: HttpRequest, product_id: int) -> HttpResponse:
+    """
+    Product page, show information about product
+    """
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'panel/product.html', context=context)
