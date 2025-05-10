14049ebb465ef4fb65bc96a1a3b1f0bc8c16cf9d
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-05-10 19:53:47 +0300

    Added test for RoleBasedView

--- panel/tests.py
+++ panel/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RoleBasedViewTests(TestCase):
+    def test_panel_page(self):
+        response = self.client.get(reverse('panel:panel'))
+        self.assertRedirects(response, reverse('users:login'))
--- tms/settings.py
+++ tms/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # --- PACKAGES FOR DEV ---
-    'django_extensions',
-    'debug_toolbar',
+    # 'django_extensions',
+    # 'debug_toolbar',
     # --- PACKAGES FOR DEV ---
     'home',
     'users',
